09b349c5ec19e434d6e7b07b067698b41bd60bfa
Kamrul Hasan <mkhrussell@gmail.com>
2026-03-11 19:53:19 -0400

    Added updated BookListViewModel to observe search query and added util extension method to convert error to text

--- shared/src/commonMain/kotlin/com/kamrul/bookcatalog/book/presentation/book_list/BookListViewModel.kt
+++ shared/src/commonMain/kotlin/com/kamrul/bookcatalog/book/presentation/book_list/BookListViewModel.kt
@@ -1,11 +1,28 @@
 package com.kamrul.bookcatalog.book.presentation.book_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kamrul.bookcatalog.book.domain.Book
+import com.kamrul.bookcatalog.book.domain.BookRepository
+import com.kamrul.bookcatalog.core.domain.onError
+import com.kamrul.bookcatalog.core.domain.onSuccess
+import com.kamrul.bookcatalog.core.domain.toUiText
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class BookListViewModel: ViewModel() {
+class BookListViewModel(
+    private val bookRepository: BookRepository
+): ViewModel() {
+    private val cachedBooks = emptyList<Book>()
+
     private val _state = MutableStateFlow(BookListState())
     val state = _state.asStateFlow()
 
@@ -23,6 +40,58 @@ class BookListViewModel: ViewModel() {
                     it.copy(selectedTabIndex = action.index)
                 }
             }
+        }
+    }
+
+    @OptIn(FlowPreview::class)
+    private fun observeSearchQuery() {
+        state
+            .map { it.searchQuery }
+            .distinctUntilChanged()
+            .debounce(500L)
+            .onEach { query ->
+                when {
+                    query.isBlank() -> {
+                        _state.update {
+                            it.copy(
+                                errorMessage = null,
+                                searchResult = cachedBooks
+                            )
+                        }
+                    }
+                    query.length >= 2 -> {
+                        searchBooks(query)
+                    }
+                }
+            }.launchIn(viewModelScope)
+    }
+
+    private fun searchBooks(query: String) {
+        _state.update {
+            it.copy(isLoading = true)
+        }
+
+        viewModelScope.launch {
+            bookRepository
+                .searchBooks(query)
+                .onSuccess { searchResult ->
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            searchResult = searchResult,
+                            errorMessage = null
+                        )
+                    }
+                }
+                .onError { error ->
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            searchResult = emptyList(),
+                            errorMessage = error.toUiText()
+                        )
+                    }
+                }
         }
     }
 }
---
+++ shared/src/commonMain/kotlin/com/kamrul/bookcatalog/core/domain/DataErrorToStringResource.kt
@@ -0,0 +1,24 @@
+package com.kamrul.bookcatalog.core.domain
+
+import bookcatalogcmp.shared.generated.resources.Res
+import bookcatalogcmp.shared.generated.resources.error_disk_full
+import bookcatalogcmp.shared.generated.resources.error_no_internet
+import bookcatalogcmp.shared.generated.resources.error_request_timeout
+import bookcatalogcmp.shared.generated.resources.error_serialization
+import bookcatalogcmp.shared.generated.resources.error_too_many_requests
+import bookcatalogcmp.shared.generated.resources.error_unknown
+import com.kamrul.bookcatalog.core.presentation.UiText
+
+fun DataError.toUiText(): UiText {
+    val stringResource = when(this) {
+        DataError.Local.DISK_FULL -> Res.string.error_disk_full
+        DataError.Local.UNKNOWN -> Res.string.error_unknown
+        DataError.Remote.REQUEST_TIMEOUT -> Res.string.error_request_timeout
+        DataError.Remote.TOO_MANY_REQUESTS -> Res.string.error_too_many_requests
+        DataError.Remote.NO_INTERNET -> Res.string.error_no_internet
+        DataError.Remote.SERVER -> Res.string.error_unknown
+        DataError.Remote.SERIALIZATION -> Res.string.error_serialization
+        DataError.Remote.UNKNOWN -> Res.string.error_unknown
+    }
+    return UiText.StringResourceId(stringResource)
+}
