4ecd09acf3f8f8b308a6813e604de3ecc98d2dcc
Kamrul Hasan <mkhrussell@gmail.com>
2026-03-10 23:08:50 -0400

    Added BookSearchBar in the BookListScreen

--- shared/src/commonMain/kotlin/com/kamrul/bookcatalog/book/presentation/book_list/BookListScreen.kt
+++ shared/src/commonMain/kotlin/com/kamrul/bookcatalog/book/presentation/book_list/BookListScreen.kt
@@ -1,10 +1,24 @@
 package com.kamrul.bookcatalog.book.presentation.book_list
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kamrul.bookcatalog.book.domain.Book
+import com.kamrul.bookcatalog.book.presentation.book_list.components.BookSearchBar
+import com.kamrul.bookcatalog.core.presentation.DarkBlue
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -30,5 +44,38 @@ private fun BooklistScreen(
     state: BookListState,
     onAction: (BookListAction) -> Unit,
 ) {
-    Text("My Booklist")
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DarkBlue)
+            .statusBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        BookSearchBar(
+            searchQuery = state.searchQuery,
+            onSearchQueryChange = { query ->
+                onAction(BookListAction.OnSearchQueryChange(query))
+            },
+            onImeSearch = {
+                keyboardController?.hide()
+            },
+            modifier = Modifier
+                .widthIn(max = 400.dp)
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+    }
+}
+
+
+@Preview
+@Composable
+private fun BooklistScreenPreview() {
+    MaterialTheme {
+        BooklistScreen(
+            state = BookListState(),
+            onAction = {}
+        )
+    }
 }
--- shared/src/commonMain/kotlin/com/kamrul/bookcatalog/book/presentation/book_list/components/BookSearchBar.kt
+++ shared/src/commonMain/kotlin/com/kamrul/bookcatalog/book/presentation/book_list/components/BookSearchBar.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
@@ -18,6 +20,7 @@ import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
@@ -38,58 +41,65 @@ fun BookSearchBar(
     onImeSearch: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    OutlinedTextField(
-        value = searchQuery,
-        onValueChange = onSearchQueryChange,
-        shape = RoundedCornerShape(100),
-        colors = OutlinedTextFieldDefaults.colors(
-            cursorColor = DarkBlue,
-            focusedBorderColor = SandYellow
-        ),
-        placeholder = {
-            Text(text = stringResource(Res.string.search_hint))
-        },
-        leadingIcon = {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
-            )
-        },
-        singleLine = true,
-        keyboardActions = KeyboardActions(
-            onSearch = {
-                onImeSearch()
-            }
-        ),
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Text,
-            imeAction = ImeAction.Search
-        ),
-        trailingIcon = {
-            AnimatedVisibility(
-                visible = searchQuery.isNotBlank()
-            ) {
-                IconButton(
-                    onClick = {
-                        onSearchQueryChange("")
-                    }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Close,
-                        contentDescription = stringResource(Res.string.close_hint),
-                        tint = MaterialTheme.colorScheme.onSurface
-                    )
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides TextSelectionColors(
+            handleColor = SandYellow,
+            backgroundColor = SandYellow
+        )
+    ) {
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = onSearchQueryChange,
+            shape = RoundedCornerShape(100),
+            colors = OutlinedTextFieldDefaults.colors(
+                cursorColor = DarkBlue,
+                focusedBorderColor = SandYellow
+            ),
+            placeholder = {
+                Text(text = stringResource(Res.string.search_hint))
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
+                )
+            },
+            singleLine = true,
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onImeSearch()
                 }
-            }
-        },
-        modifier = modifier
-            .background(
-                shape = RoundedCornerShape(100),
-                color = DesertWhite
-            )
-            .minimumInteractiveComponentSize()
-    )
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
+            trailingIcon = {
+                AnimatedVisibility(
+                    visible = searchQuery.isNotBlank()
+                ) {
+                    IconButton(
+                        onClick = {
+                            onSearchQueryChange("")
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = stringResource(Res.string.close_hint),
+                            tint = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                }
+            },
+            modifier = modifier
+                .background(
+                    shape = RoundedCornerShape(100),
+                    color = DesertWhite
+                )
+                .minimumInteractiveComponentSize()
+        )
+    }
 }
 
 @Preview
