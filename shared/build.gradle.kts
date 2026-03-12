e072aa474ad7c6dabff46982e4fb3f42f04aec6f
Kamrul Hasan <mkhrussell@gmail.com>
2026-03-12 00:46:42 -0400

    Remved unused codes

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -81,8 +81,6 @@ kotlin {
         implementation(libs.androidx.lifecycle.viewmodelCompose)
         implementation(libs.androidx.lifecycle.runtimeCompose)
 
-        implementation(libs.kotlinx.datetime)
-
         implementation(libs.koin.compose)
         implementation(libs.koin.compose.viewmodel)
         api(libs.koin.core)
--- webApp/src/webMain/kotlin/com/kamrul/bookcatalog/main.kt
+++ webApp/src/webMain/kotlin/com/kamrul/bookcatalog/main.kt
@@ -1,20 +1,14 @@
 package com.kamrul.bookcatalog
 
-import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import com.kamrul.bookcatalog.di.initKoin
-import io.ktor.client.engine.js.Js
 import kotlin.js.ExperimentalWasmJsInterop
-import kotlin.js.JsModule
 
-@OptIn(ExperimentalWasmJsInterop::class)
-@JsModule("@js-joda/timezone")
-external object JsJodaTimeZoneModule
-
-private val jsJodaTz = JsJodaTimeZoneModule
-
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(
+    ExperimentalWasmJsInterop::class,
+    ExperimentalComposeUiApi::class
+)
 fun main() {
     initKoin()
     ComposeViewport {
