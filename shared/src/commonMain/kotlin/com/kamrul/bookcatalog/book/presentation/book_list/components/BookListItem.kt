a0d54c8a92a8600c3023e2fb6e40da39339a3a26
Kamrul Hasan <mkhrussell@gmail.com>
2026-03-12 22:00:57 -0400

    Added Rotation animation in image loading in BookListItem

--- shared/src/commonMain/kotlin/com/kamrul/bookcatalog/book/presentation/book_list/components/BookListItem.kt
+++ shared/src/commonMain/kotlin/com/kamrul/bookcatalog/book/presentation/book_list/components/BookListItem.kt
@@ -1,5 +1,7 @@
 package com.kamrul.bookcatalog.book.presentation.book_list.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -27,12 +29,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import bookcatalogcmp.shared.generated.resources.Res
 import bookcatalogcmp.shared.generated.resources.book_error
+import coil3.compose.AsyncImagePainter
 import coil3.compose.rememberAsyncImagePainter
 import com.kamrul.bookcatalog.book.domain.Book
 import com.kamrul.bookcatalog.core.presentation.LightBlue
@@ -83,6 +88,12 @@ fun BookListItem(
                     }
                 )
 
+                val painterState = painter.state.collectAsStateWithLifecycle()
+                val transition by animateFloatAsState(
+                    targetValue = if (painterState.value is AsyncImagePainter.State.Success) 1f else 0f,
+                    animationSpec = tween(durationMillis = 800)
+                )
+
                 when (val result = imageLoadResult) {
                     null -> {
                         PulseAnimation(
@@ -100,6 +111,12 @@ fun BookListItem(
                                     ratio = 0.65f,
                                     matchHeightConstraintsFirst = true
                                 )
+                                .graphicsLayer {
+                                    rotationX = (1f - transition) * 30f
+                                    val scale = 0.8f + (0.2f * transition)
+                                    scaleX = scale
+                                    scaleY = scale
+                                }
                         )
                     }
                 }
