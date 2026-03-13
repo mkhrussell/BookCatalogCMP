bd8d0db709a58f31635ca3ffa7d10815f6c4164e
Kamrul Hasan <mkhrussell@gmail.com>
2026-03-12 20:50:34 -0400

    Added database access to repository

--- shared/src/commonMain/kotlin/com/kamrul/bookcatalog/book/data/repository/DefaultBookRepository.kt
+++ shared/src/commonMain/kotlin/com/kamrul/bookcatalog/book/data/repository/DefaultBookRepository.kt
@@ -1,15 +1,22 @@
 package com.kamrul.bookcatalog.book.data.repository
 
+import androidx.sqlite.SQLiteException
+import com.kamrul.bookcatalog.book.data.database.FavoriteBookDao
 import com.kamrul.bookcatalog.book.data.mappers.toBook
+import com.kamrul.bookcatalog.book.data.mappers.toBookEntity
 import com.kamrul.bookcatalog.book.data.network.RemoteBookDataSource
 import com.kamrul.bookcatalog.book.domain.Book
 import com.kamrul.bookcatalog.book.domain.BookRepository
 import com.kamrul.bookcatalog.core.domain.DataError
+import com.kamrul.bookcatalog.core.domain.EmptyResult
 import com.kamrul.bookcatalog.core.domain.Result
 import com.kamrul.bookcatalog.core.domain.map
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class DefaultBookRepository(
     private val remoteBookDataSource: RemoteBookDataSource,
+    private val favoriteBookDao: FavoriteBookDao
 ): BookRepository {
     override suspend fun searchBooks(
         query: String
@@ -25,5 +32,35 @@ class DefaultBookRepository(
         return remoteBookDataSource
             .getBookDetails(bookId)
             .map { it.description }
+    }
+
+    override fun getFavoriteBooks(): Flow<List<Book>> {
+        return favoriteBookDao
+            .getFavoriteBooks()
+            .map { bookEntities ->
+                bookEntities.map { it.toBook() }
+            }
+    }
+
+    override fun isBookFavorite(id: String): Flow<Boolean> {
+        return favoriteBookDao
+            .getFavoriteBooks()
+            .map { bookEntities ->
+                bookEntities.any { it.id == id }
+            }
+    }
+
+    override suspend fun markAsFavorite(book: Book): EmptyResult<DataError.Local> {
+        return try {
+            favoriteBookDao.upsertFavoriteBook(book.toBookEntity())
+
+            Result.Success(Unit)
+        } catch (e: SQLiteException) {
+            Result.Error(DataError.Local.DISK_FULL)
+        }
+    }
+
+    override suspend fun deleteFromFavorite(id: String) {
+        favoriteBookDao.deleteFavoriteBook(id)
     }
 }
--- shared/src/commonMain/kotlin/com/kamrul/bookcatalog/book/domain/BookRepository.kt
+++ shared/src/commonMain/kotlin/com/kamrul/bookcatalog/book/domain/BookRepository.kt
@@ -1,7 +1,9 @@
 package com.kamrul.bookcatalog.book.domain
 
 import com.kamrul.bookcatalog.core.domain.DataError
+import com.kamrul.bookcatalog.core.domain.EmptyResult
 import com.kamrul.bookcatalog.core.domain.Result
+import kotlinx.coroutines.flow.Flow
 
 interface BookRepository {
     suspend fun searchBooks(
@@ -11,4 +13,9 @@ interface BookRepository {
     suspend fun getBookDeDescription(
         bookId: String
     ): Result<String?, DataError>
+
+    fun getFavoriteBooks(): Flow<List<Book>>
+    fun isBookFavorite(id: String): Flow<Boolean>
+    suspend fun markAsFavorite(book: Book): EmptyResult<DataError.Local>
+    suspend fun deleteFromFavorite(id: String)
 }
